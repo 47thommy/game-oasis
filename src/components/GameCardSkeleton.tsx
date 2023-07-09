4fe84bee93dfbfb2f82cc8226d6b267c32d750b1
47thommy <47thommy@gmail.com>
2023-07-09 13:56:09 +0300

    fixed alignment issue with the skeleton and the game card

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card>
+    <Card borderRadius={10} overflow="hidden" width="300px">
       <Skeleton height="200px "></Skeleton>
       <CardBody>
         <SkeletonText></SkeletonText>
