5e2d76ae058e9fa845951aea268ec6a3325c3882
47thommy <47thommy@gmail.com>
2023-07-09 16:33:43 +0300

    added background image to each genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres";
+import getCroppedImage from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenre();
   return (
-    <div>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImage(genre.image_background)}
+            ></Image>
+            <Text>{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </div>
+    </List>
   );
 };
 
