a7cb3f81de063e408eb26bfdca08ed6ee86be95e
47thommy <47thommy@gmail.com>
2023-07-09 17:35:41 +0300

    show a circular loading while fetching for genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,10 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenres";
 import getCroppedImage from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenre();
+  const { data, isLoading } = useGenre();
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {data.map((genre) => (
